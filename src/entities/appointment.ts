798da489ce72079af339c9e4b426b6692d0e425e
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-04-24 11:34:21 -0300

    :sparkles: create: getters - Appointment (@entities)

--- src/entities/appointment.ts
+++ src/entities/appointment.ts
@@ -3,5 +3,11 @@ import { IAppointmentProps } from "@Types";
 export class Appointment {
   private props: IAppointmentProps
 
- constructor (props:IAppointmentProps){ this.props = props }  
+  constructor (props:IAppointmentProps){ this.props = props }  
+
+  get customer () { return this.props.costumer }
+
+  get startsAt () { return this.props.startsAt }
+
+  get endsAt () { return this.props.endsAt }
 }
