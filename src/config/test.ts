74edcd23197abee91bc578ff2ab9572e8cdf7753
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-04-24 11:48:54 -0300

    :gear: create: const - VALID_APPOINTMENT_DATA

--- src/config/test.ts
+++ src/config/test.ts
@@ -1,6 +1,18 @@
 import { IAppointmentProps } from "@Types";
 import { Appointment } from "@/entities/appointment";
 
+export const VALID_APPOINTMENT_DATA = () => {
 
+  const startsAt = new Date();
+  const endsAt = new Date()
+
+  endsAt.setDate(startsAt.getDate()+0)
+
+  return {
+    customer: 'John Doe',
+    startsAt,
+    endsAt,
+  } as IAppointmentProps
+}
 
 export const VALID_APPOINTMENT = () => new Appointment(VALID_APPOINTMENT_DATA())
