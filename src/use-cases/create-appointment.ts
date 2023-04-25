5883ca355a6c6d18ac897654a2b7df3895ac00aa
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-04-25 08:57:19 -0300

    :sparkles: update: use case - create appointment

--- src/use-cases/create-appointment.ts
+++ src/use-cases/create-appointment.ts
@@ -1,18 +1,32 @@
 import { CreateAppointmentRespose, ICreateAppopintmentRequest } from "@Types";
 import { Appointment } from "@Entities/appointment";
+import { IAppointmentsRepository } from "@/repositories/appointments-repository";
 
 export class CreateAppointment {
+
+  constructor (
+    private appointmentRepository: IAppointmentsRepository
+  ) {}
+
   async execute ({
     customer,
     startsAt,
     endsAt,
   }: ICreateAppopintmentRequest): Promise<CreateAppointmentRespose> {
 
+    const overlappingAppointment = await this
+        .appointmentRepository
+        .findOverlappingAppointment(startsAt, endsAt)
+
+    if (overlappingAppointment) throw new Error('another appointment overlap this appointment')
+
     const appointment = new Appointment ({
       customer,
       startsAt,
       endsAt,
     })
+
+    await this.appointmentRepository.create(appointment)
 
     return appointment;
   }
